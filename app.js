a303b748ae940a93cc7d923f4f130679d708d041
Anas MASSNAOUI <anasmassnaoui@gmail.com>
2021-09-12 16:55:50 +0100

    use cors & sockets
    - enable cors
    - link sockets with express

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
 // for handling routes
 const express = require('express');
 const path = require('path');
-
+// for cors handling
+const cors = require('cors')
+// for socket handling
+const expressWs = require('express-ws')
 // for requests log
 const logger = require('morgan');
 
-// for subroutes use
-const api_router = require('./routes/index');
-
 const app = express();
+// link sockets with express
+const wsapp = expressWs(app)
 
+// export express socket to be used by sub routes
+module.exports.wsapp = wsapp
+
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// for subroutes use
+const api_router = require('./routes/index');
 /* map api routes. */
 app.use('/api', api_router);
 
-module.exports = app;
+module.exports = app
